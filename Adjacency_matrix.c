13321d73276a572e3c8f5afd7a33ea0cbfaf79bc
diu-algo-181-35-2408 <49320864+diu-algo-181-35-2408@users.noreply.github.com>
2019-04-06 07:41:55 -0700

    Update Adjacency_matrix.c

--- Adjacency_matrix.c
+++ Adjacency_matrix.c
@@ -3,7 +3,8 @@ ID:181-35-2408
 ****/
 
 #include<stdio.h>
-int matrix[7][7]={
+#define size 7
+int matrix[size][size]={
     {0,1,1,1,0,0,0},
     {0,0,0,0,1,0,0},
     {0,0,0,1,0,1,0},
@@ -17,9 +18,9 @@ int main()
 {
     int i=0,j=0;
     printf("Vartex &  Edge : \n\n");
-    for(i=0; i<7; i++)
+    for(i=0; i<size; i++)
     {
-        for(j=0; j<7; j++)
+        for(j=0; j<size; j++)
         {
             if(matrix[i][j]==1)
                 printf("%d --- %d\n",i,j);
